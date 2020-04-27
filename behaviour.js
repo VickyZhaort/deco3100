8f023e5a317a2c15f466db59a46f51cfce02dafe
VickyZhaort <64390009+VickyZhaort@users.noreply.github.com>
2020-04-28 01:15:41 +1000

    Add files via upload

---
+++ behaviour.js
@@ -0,0 +1,14 @@
+ 
+var a = 10;
+var b = 3;
+var c = a + b;
+console.log(c);
+
+function changeText() {
+  document.getElementById("first-heading").textContent = "Header changed!";
+}
+
+function removeClass(){
+var element = document.getElementById("paragraph1");
+element.classList.remove('special-text');
+}
